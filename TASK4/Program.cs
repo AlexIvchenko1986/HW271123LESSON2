2f3ac876675af94eb49acbb4a3ae5ab988faa0e0
Alexander Ivchenko <alex1986br@yandex.ru>
2023-11-27 19:21:47 +0300

    ADD TASK4 not completed

---
+++ TASK4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 4: Напишите программу, которая на вход
+// принимает натуральное число N, а на выходе
+// показывает его цифры через запятую.
+
+// 568 => 5,6,8
+// 8 => 8
+// 9542 => 9,5,4,2
+
+Console.WriteLine("Введите целое число от 1 до N ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+
+Console.WriteLine ($" => {number}");
+
+//int count = 0;
+//int index = 0;
+
+// while (true)
+// {
+// 	if (number % 2 == 0)
+// 	{
+// 		Console.Write(index + " ");
+// 		count++;
+//     }
+// }
+
+
